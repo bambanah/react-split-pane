e2039819130c9cc0828223970a04c064a9c5cf9b
Lachie Underhill <lachlanu@gmail.com>
2024-05-31 12:09:44 +1000

    Readd (broken) tests

--- test/assertions/Asserter.js
+++ test/assertions/Asserter.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import SplitPane from '../../src/SplitPane';
-import Pane from '../../src/Pane';
-import Resizer from '../../src/Resizer';
+import React from "react";
+import SplitPane, { Pane, Resizer } from "../../";
 import {
   findRenderedComponentWithType,
   scryRenderedComponentsWithType,
-} from 'react-dom/test-utils';
-import { findDOMNode } from 'react-dom';
-import chai from 'chai';
+} from "react-dom/test-utils";
+import { findDOMNode } from "react-dom";
+import chai from "chai";
 
-import {calculatePointsBetween, getCentre, renderComponent, unmountComponent} from '../lib/utils';
+import {
+  calculatePointsBetween,
+  getCentre,
+  renderComponent,
+  unmountComponent,
+} from "../lib/utils";
 
 const expect = chai.expect;
 
 const debug = false;
 
 const log = (...args) => {
-  if (debug) console.log(...['Asserter', ...args]);
+  if (debug) console.log(...["Asserter", ...args]);
 };
 
-
-
-
-
 //.....
-
 
 const asserter = (jsx, dimensions = {}) => {
   const splitPane = renderComponent(jsx, dimensions);
@@ -33,18 +31,14 @@ const asserter = (jsx, dimensions = {}) => {
   const findPanes = () => {
     log(`findPanes`);
     const components = scryRenderedComponentsWithType(component, Pane);
-    components.forEach(_ =>
-      log(findDOMNode(_).getBoundingClientRect())
-    );
+    components.forEach((_) => log(findDOMNode(_).getBoundingClientRect()));
     return components;
   };
 
   const findResizers = () => {
     log(`findResizers`);
     const components = scryRenderedComponentsWithType(component, Resizer);
-    components.forEach(_ =>
-      log(findDOMNode(_).getBoundingClientRect())
-    );
+    components.forEach((_) => log(findDOMNode(_).getBoundingClientRect()));
     return components;
   };
 
@@ -84,77 +78,87 @@ const asserter = (jsx, dimensions = {}) => {
   return {
     assertNumberOfPanes(expected) {
       const panes = findPanes();
-      expect(panes.length).to.equal(expected, 'Unexpected number of panes');
+      expect(panes.length).to.equal(expected, "Unexpected number of panes");
       return this;
     },
     assertNumberOfResizers(expected) {
       const resizers = findResizers();
       expect(resizers.length).to.equal(
         expected,
-        'Unexpected number of resizers'
+        "Unexpected number of resizers"
       );
       return this;
     },
     assertOrientation(expected) {
-      expect(component.props['split']).to.eql(
+      expect(component.props["split"]).to.eql(
         expected,
-        'Unexpected split orientation'
+        "Unexpected split orientation"
       );
       return this;
     },
     assertRatios(expected) {
       const panes = scryRenderedComponentsWithType(component, Pane);
-      const actualSizes = panes.map(_ => +_.props['ratio']);
-      expect(actualSizes).to.eql(expected, 'Unexpected ratios');
+      const actualSizes = panes.map((_) => +_.props["ratio"]);
+      expect(actualSizes).to.eql(expected, "Unexpected ratios");
       return this;
     },
     assertSizes(expected) {
       const panes = scryRenderedComponentsWithType(component, Pane);
-      const actualSizes = panes.map(_ => _.props['size']);
-      expect(actualSizes).to.eql(expected, 'Unexpected sizes');
+      const actualSizes = panes.map((_) => _.props["size"]);
+      expect(actualSizes).to.eql(expected, "Unexpected sizes");
       return this;
     },
     assertSizesPx(expected) {
       const panes = scryRenderedComponentsWithType(component, Pane);
-      const actualSizes = panes.map(_ => {
-        const sizeProp = component.props['split'] === 'vertical' ? 'width' : 'height';
+      const actualSizes = panes.map((_) => {
+        const sizeProp =
+          component.props["split"] === "vertical" ? "width" : "height";
         return findDOMNode(_).getBoundingClientRect()[sizeProp];
       });
-      expect(actualSizes).to.eql(expected, 'Unexpected sizes in px');
+      expect(actualSizes).to.eql(expected, "Unexpected sizes in px");
       return this;
     },
 
     // todo - are these relevant?
     assertMinSizes(expected) {
       const panes = scryRenderedComponentsWithType(component, Pane);
-      const actualSizes = panes.map(_ => _.props['minSize']);
-      expect(actualSizes).to.eql(expected, 'Unexpected min size');
+      const actualSizes = panes.map((_) => _.props["minSize"]);
+      expect(actualSizes).to.eql(expected, "Unexpected min size");
       return this;
     },
     assertMaxSizes(expected) {
       const panes = scryRenderedComponentsWithType(component, Pane);
-      const actualSizes = panes.map(_ => _.props['maxSize']);
-      expect(actualSizes).to.eql(expected, 'Unexpected max size');
+      const actualSizes = panes.map((_) => _.props["maxSize"]);
+      expect(actualSizes).to.eql(expected, "Unexpected max size");
       return this;
     },
 
     assertFlexSizes(expectedSizes) {
       const panes = scryRenderedComponentsWithType(component, Pane);
-      const actualSizes = panes.map(_ => +findDOMNode(_).style.flexGrow);
-      expect(actualSizes).to.eql(expectedSizes, 'Unexpected flex sizes');
+      const actualSizes = panes.map((_) => +findDOMNode(_).style.flexGrow);
+      expect(actualSizes).to.eql(expectedSizes, "Unexpected flex sizes");
       return this;
     },
-    dragResizer(resizerIndex, mousePositionDifference, mouseRightClick = false) {
-      const coordinates = calculateMouseMove(resizerIndex, mousePositionDifference);
+    dragResizer(
+      resizerIndex,
+      mousePositionDifference,
+      mouseRightClick = false
+    ) {
+      const coordinates = calculateMouseMove(
+        resizerIndex,
+        mousePositionDifference
+      );
       const [startPosition, ...moveCoordinates] = coordinates;
       const event = {
-        preventDefault(){},
+        preventDefault() {},
         button: mouseRightClick ? 1 : 0,
-        ...startPosition
+        ...startPosition,
       };
 
       component.onMouseDown(event, resizerIndex);
-      moveCoordinates.forEach(coordinate => component.onMouseMove({...coordinate, preventDefault(){}}));
+      moveCoordinates.forEach((coordinate) =>
+        component.onMouseMove({ ...coordinate, preventDefault() {} })
+      );
       component.onMouseUp(event);
       return this;
     },
