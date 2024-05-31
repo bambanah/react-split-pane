2d0e6aacb2e8d13130fafb014d2e1172114f9b52
Lachie Underhill <lachlanu@gmail.com>
2024-05-31 12:20:32 +1000

    Remove dist folder

--- dist/Resizer.d.ts
+++
@@ -1,8 +0,0 @@
-import { default as React } from 'react';
-
-interface ResizerProps extends React.HTMLAttributes<HTMLDivElement> {
-    index: number;
-    split: "vertical" | "horizontal";
-}
-declare const Resizer: ({ index, split, ...rest }: ResizerProps) => import("react/jsx-runtime").JSX.Element;
-export default Resizer;
--- dist/main.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as Pane } from './Pane';
-export { default as Resizer } from './Resizer';
-export { default } from './SplitPane';
